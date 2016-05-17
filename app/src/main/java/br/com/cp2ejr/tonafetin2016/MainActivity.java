e08abc58b02d3139e004b152fd41d50776c8cc6f
Leonardo Saldanha <leoosaldanha@gmail.com>
2016-05-17 08:06:32 -0300

    Adicionando o Facebook SDK ao projeto

--- app/src/main/java/br/com/cp2ejr/tonafetin2016/MainActivity.java
+++ app/src/main/java/br/com/cp2ejr/tonafetin2016/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
